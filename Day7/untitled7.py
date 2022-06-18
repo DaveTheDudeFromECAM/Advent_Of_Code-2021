e684de03dac27b2bacb916aea25f95da1861ec56
DaveTheDudeFromECAM <20194@ecam.be>
2022-06-19 01:09:55 +0200

    .

--- Day7/untitled7.py
+++
@@ -1,8 +0,0 @@
-with open("input.txt") as f: 
-    data = [int(x) for x in f.read().strip().split(",")]
-    part_one = min([ sum([max([x,i]) - min([x,i]) for x in data]) for i in range(min(data), max(data))])
-    part_two = min([ sum([ sum(range(1 + max([x,i]) - min([x,i]))) for x in data]) for i in range(min(data), max(data))])
-print("part_one \n")
-print(part_one)
-print("\n part_two \n")
-print(part_two)
